00398605659253efaf17be5729ae159ac910af82
naeohmi <naeohmi@gmail.com>
2017-05-24 10:42:19 -0400

    api data returned and db structure

--- app/routes/Nutritionix.js
+++ app/routes/Nutritionix.js
@@ -1,30 +1,21 @@
 var config = require('../config.js');
 
-class Nutritionix {
-    constructor() {}
-    firstCall(upc) {
+let getInfo = (upc) => {
         return config.axios.get(`https://api.nutritionix.com/v1_1/item?upc=${upc}&appId=463fe736&appKey=8c512edcb05e0b6539ff12807328a4b6`)
-    };
-    secondCall() {
-        var configHeader = {
-            headers: {
-            }
-        };
-        return config.axios.get(``, configHeader)
-    };
-    allCall() {
-        config.axios.all([this.firstCall(), this.secondCall()])
-            .then(config.axios.spread((first, second) => {
-                var data1 = first.data.etc;
-                var data2 = second.data.etc;
+            .then((res) => {
+                console.log('res: ' res);
+                var ingredientName = res.data.item_name;
+                var ingredientList = res.data.nf_ingredient_statement;
+                console.log('name: ' ingredientName)
+                console.log('list: ' ingredientList)
 
                 config.db.none(
-                        "INSERT INTO DB (data1, data2)" +
-                        "VALUES ($1, $2);", [data1, data2]
+                        "INSERT INTO DB (upc, name, ingredient)" +
+                        "VALUES ($1, $2, $3);", [upc, ingredientName, ingredientList]
                     )
             }))
             .catch((err) => {
-                console.log(err);
+                return next(err);
             });
     };
 };
