1edd7f5fcfabeedf2b5f1aa030832fb9ff220007
Esraa Alaarag <m_israa1981@hotmail.com>
2017-05-27 18:08:53 -0400

    the number validator is done

--- react/src/components/Result.js
+++ react/src/components/Result.js
@@ -48,14 +48,6 @@ class Result extends Component {
         //console.log(this.state.productName);
         //console.log(this.state.ingredientList);
          //console.log(this.state.ingredientString);
-         this.setState((previousState) => {
-            previousState.findings.barcode = this.props.upc;
-            return previousState;
-          });
-         this.setState((previousState) => {
-            previousState.findings.product = this.props.productName;
-            return previousState;
-          });
          this.filter()
       });
           }
@@ -74,6 +66,9 @@ class Result extends Component {
   }
 
 
+
+
+
  search(query) {
   return this.state.ingredientList.filter(function(el) {
       return el.toLowerCase().indexOf(query.toLowerCase()) > -1;
@@ -86,6 +81,16 @@ analysingresult(){
 };
 
   filter(){
+    let eggs= false;
+    let fish= false;
+    let milk= false;
+    let peanuts= false;
+    let sesame= false;
+    let shellfish= false;
+    let soy= false;
+    let treenuts= false;
+    let wheat= false;
+    let result=false;
  console.log("I'm filtering")
    
 
@@ -93,190 +98,358 @@ analysingresult(){
       console.log("user with egg allergy")
       if(this.search('egg').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.eggs(true)}
+        eggs=true;
+        result=true;
+      }
       else if(this.search('Mayonnaise').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.eggs(true)}
+        eggs=true;
+         result=true;
+      }
       else
-        console.log("you can eat that")
+        console.log("you can eat that");
     }
    if(this.state.issues["fishallergy"]){
       console.log("user with fish allergy")
       if(this.search('Catfish').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.fish(true)}
+        fish=true;
+         result=true;
+       }
       else if(this.search('fish').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.fish(true)}
+        fish=true;
+         result=true;
+          }
       else if(this.search('Bass').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.fish(true)}
+        fish=true;
+         result=true;
+          }
       else if(this.search('Cod').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.fish(true)}
+        fish=true;
+         result=true;
+       }
       else if(this.search('Anchovies').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.fish(true)}
+        fish=true;
+         result=true;
+          }
       else if(this.search('Flounder').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.fish(true)}
+        fish=true;
+         result=true;
+          }
       else if(this.search('Grouper').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.fish(true)}
+        fish=true;
+         result=true;
+       }
       else if(this.search('Haddock').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.fish(true)}
+        fish=true;
+         result=true;
+          }
       else if(this.search('Hake').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.fish(true)}
+        fish=true;
+         result=true;
+          }
       else if(this.search('Halibut').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.fish(true)}
+        fish=true;
+         result=true;
+          }
       else if(this.search('Herring').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.fish(true)}
+        fish=true;
+         result=true;
+          }
       else if(this.search('Mahi').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.fish(true)}
+        fish=true;
+         result=true;
+          }
       else if(this.search('Perch').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.fish(true)}
+        fish=true;
+         result=true;
+       }
       else if(this.search('Pike').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.fish(true)}
+        fish=true;
+         result=true;
+          }
       else if(this.search('Pollock').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.fish(true)}
+        fish=true;
+         result=true;
+          }
       else if(this.search('Salmon').length !== 0){
-        console.log("donot eat it")
-        this.setState.findings.fish(true)}
+        console.log("donot eat it");
+        fish=true;
+         result=true;
+          }
       else if(this.search('Swordfish').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.fish(true)}
+        fish=true;
+         result=true;
+          }
       else if(this.search('Sole').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.fish(true)}
+        fish=true;
+         result=true;
+          }
       else if(this.search('Snapper').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.fish(true)}
+        fish=true;
+         result=true;
+          }
        else if(this.search('Tilapia').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.fish(true)}
+        fish=true;
+         result=true;
+          }
       else if(this.search('Trout').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.fish(true)}
+        fish=true;
+         result=true;
+          }
       else if(this.search('Tuna').length !== 0){
         console.log("donot eat it")
-        this.setState.findings.fish(true)}
+        fish=true;
+         result=true;
+          }
       else
         console.log("you can eat that");
      
     }
      if(this.state.issues["milkallergy"]){
      console.log("user with milk allergy")
-      if(this.search('milk').length !== 0)
-        console.log("donot eat it")
-      else if(this.search('Butter').length !== 0)
-        console.log("donot eat it")
-      else if(this.search('Cheese').length !== 0)
-        console.log("donot eat it")
-      else if(this.search('Cream').length !== 0)
-        console.log("donot eat it")
-      else if(this.search('Ghee').length !== 0)
-        console.log("donot eat it")
-      else if(this.search('Yogurt').length !== 0)
-        console.log("donot eat it")
+      if(this.search('milk').length !== 0){
+        console.log("donot eat it");
+        milk=true;
+         result=true;
+      }
+      else if(this.search('Butter').length !== 0){
+        console.log("donot eat it");
+        milk=true;
+         result=true;
+      }
+      else if(this.search('Cheese').length !== 0){
+        console.log("donot eat it");
+        milk=true;
+         result=true;
+      }
+      else if(this.search('Cream').length !== 0){
+        console.log("donot eat it");
+        milk=true;
+         result=true;
+      }
+      else if(this.search('Ghee').length !== 0){
+        console.log("donot eat it");
+        milk=true;
+         result=true;
+      }
+      else if(this.search('Yogurt').length !== 0){
+        console.log("donot eat it");
+        milk=true;
+         result=true;
+      }
       else
         console.log("you can eat that")
     }
      if(this.state.issues["peanutsallergy"]){
       console.log("user with peanut allergy")
-      if(this.search('peanut').length !== 0)
-        console.log("donot eat it")
-      else if(this.search('nut').length !== 0)
-        console.log("donot eat it")
+      if(this.search('peanut').length !== 0){
+        console.log("donot eat it");
+        peanuts=true;
+         result=true;
+      }
+      else if(this.search('nut').length !== 0){
+        console.log("donot eat it");
+        peanuts=true;
+         result=true;
+      }
       else
         console.log("you can eat that")
     }
      if(this.state.issues["sesameallergy"]){
      console.log("user with sesame allergy")
-      if(this.search('sesame').length !== 0)
-        console.log("donot eat it")
-      else if(this.search('Tahini').length !== 0)
-        console.log("donot eat it")
+      if(this.search('sesame').length !== 0){
+        console.log("donot eat it");
+        sesame=true;
+         result=true;
+      }
+      else if(this.search('Tahini').length !== 0){
+        console.log("donot eat it");
+        sesame=true;
+         result=true;
+      }
       else
         console.log("you can eat that")
     }
      if(this.state.issues["soyallergy"]){
       console.log("user with Soy allergy")
-      if(this.search('soy').length !== 0)
-        console.log("donot eat it")
-      else if(this.search('miso').length !== 0)
-        console.log("donot eat it")
-      else if(this.search('Edamame').length !== 0)
-        console.log("donot eat it")
-      else if(this.search('Tofu').length !== 0)
-        console.log("donot eat it")
+      if(this.search('soy').length !== 0){
+        console.log("donot eat it");
+        soy=true;
+         result=true;
+      }
+      else if(this.search('miso').length !== 0){
+        console.log("donot eat it");
+        soy=true;
+         result=true;
+      }
+      else if(this.search('Edamame').length !== 0){
+        console.log("donot eat it");
+        soy=true;
+         result=true;
+      }
+      else if(this.search('Tofu').length !== 0){
+        console.log("donot eat it");
+        soy=true;
+         result=true;
+      }
       else
         console.log("you can eat that")
        }
      if(this.state.issues["treenutsallergy"]){
       console.log("user with tree nut allergy")
-      if(this.search('Almond').length !== 0)
+      if(this.search('Almond').length !== 0){
         console.log("donot eat it")
-      else if(this.search('Cashew').length !== 0)
+        treenuts=true;
+         result=true;
+      }
+      else if(this.search('Cashew').length !== 0){
+        console.log("donot eat it");
+         treenuts=true;
+          result=true;
+      }
+      else if(this.search('Chestnut').length !== 0){
+        console.log("donot eat it");
+         treenuts=true;
+          result=true;
+      }
+      else if(this.search('Hazelnut').length !== 0){
+        console.log("donot eat it");
+         treenuts=true;
+          result=true;
+      }
+      else if(this.search('Macadamia').length !== 0){
+        console.log("donot eat it");
+         treenuts=true;
+          result=true;
+      }
+      else if(this.search('Pecan').length !== 0){
         console.log("donot eat it")
-      else if(this.search('Chestnut').length !== 0)
+         treenuts=true;
+          result=true;
+      }
+      else if(this.search('Pine').length !== 0){
         console.log("donot eat it")
-      else if(this.search('Hazelnut').length !== 0)
+         treenuts=true;
+          result=true;
+      }
+      else if(this.search('Pistachio').length !== 0){
         console.log("donot eat it")
-      else if(this.search('Macadamia').length !== 0)
+         treenuts=true;
+          result=true;
+      }
+      else if(this.search('Walnut').length !== 0){
         console.log("donot eat it")
-      else if(this.search('Pecan').length !== 0)
-        console.log("donot eat it")
-      else if(this.search('Pine').length !== 0)
-        console.log("donot eat it")
-      else if(this.search('Pistachio').length !== 0)
-        console.log("donot eat it")
-      else if(this.search('Walnut').length !== 0)
-        console.log("donot eat it")
+         treenuts=true;
+          result=true;
+      }
       else
         console.log("you can eat that")
        }
     
       if(this.state.issues["wheatsallergy"]){
         console.log("user with wheat allergy")
-        if(this.search('wheat').length !== 0)
-        console.log("donot eat it")
-        else if(this.search('Bread').length !== 0)
+        if(this.search('wheat').length !== 0){
+        console.log("donot eat it") ; 
+        wheat=true;
+        result=true;
+        
+      }
+        else if(this.search('Bread').length !== 0){
           console.log("donot eat it")
-        else if(this.search('Bulgur').length !== 0)
+          wheat=true;
+        result=true;
+        }
+        else if(this.search('Bulgur').length !== 0){
           console.log("donot eat it")
-        else if(this.search('Couscous').length !== 0)
+          wheat=true;
+        result=true;
+        }
+        else if(this.search('Couscous').length !== 0){
           console.log("donot eat it")
+          wheat=true;
+        result=true;
+        }
          else
             console.log("you can eat that")
         }
      if(this.state.issues["shellfishallergy"]){
       console.log("user with Shell fish allergy")
-      if(this.search('Barnacle').length !== 0)
-        console.log("donot eat it")
-      else if(this.search('Crab').length !== 0)
-        console.log("donot eat it")
-      else if(this.search('Crawfish').length !== 0)
-        console.log("donot eat it")
-      else if(this.search('Krill').length !== 0)
-        console.log("donot eat it")
-      else if(this.search('Lobster').length !== 0)
-        console.log("donot eat it")
-      else if(this.search('Prawns').length !== 0)
-        console.log("donot eat it")
-      else if(this.search('Shrimp').length !== 0)
-        console.log("donot eat it")
+      if(this.search('Barnacle').length !== 0){
+        console.log("donot eat it");
+        shellfish=true;
+        result=true;
+      }
+      else if(this.search('Crab').length !== 0){
+        console.log("donot eat it");
+        shellfish=true;
+        result=true;
+      }
+      else if(this.search('Crawfish').length !== 0){
+        console.log("donot eat it");
+        shellfish=true;
+        result=true;
+      }
+      else if(this.search('Krill').length !== 0){
+        console.log("donot eat it");
+        shellfish=true;
+        result=true;
+      }
+      else if(this.search('Lobster').length !== 0){
+        console.log("donot eat it");
+        shellfish=true;
+        result=true;
+      }
+      else if(this.search('Prawns').length !== 0){
+        console.log("donot eat it");
+        shellfish=true;
+        result=true;
+      }
+      else if(this.search('Shrimp').length !== 0){
+        console.log("donot eat it");
+        shellfish=true;
+        result=true;
+      }
       else
         console.log("you can eat that")
       }
+      console.log(this.state.findings);
+          this.setState((previousState) => {
+            previousState.findings.eggs = eggs;
+            previousState.findings.milk = milk;
+            previousState.findings.fish = fish;
+            previousState.findings.peanuts = peanuts;
+            previousState.findings.sesame = sesame;
+            previousState.findings.shellfish = shellfish;
+            previousState.findings.soy = soy;
+            previousState.findings.treenuts = treenuts;
+            previousState.findings.wheat = wheat;
+            previousState.findings.result = result;
+            previousState.findings.barcode = this.props.upc;
+            previousState.findings.product = this.props.productName;
+            return previousState;
+          });
   }
+
   componentDidMount(){
 this.userPref();
 
@@ -287,14 +460,13 @@ this.userPref();
   render() {
     var ing = this.state.ingredientString;
     var Name = this.state.findings.product;
-    var barcode=this.state.findings.barcode;
     var productBrand=this.state.productBrand;
-    var result
     return (
            <div className="resultContainer">
               <div className="result">
                 <div className="resultTable">
                   <table>
+                   <tbody>
                     <tr>
                       <td><h2>Product Name:</h2></td>
                       <td><h4>{Name}</h4></td>
@@ -303,6 +475,7 @@ this.userPref();
                       <td><h2>Product Brand:</h2></td>
                       <td><h4>{productBrand}</h4></td>
                     </tr>
+                     </tbody>
                    </table>
                   </div>
                  <div className="productPic">
